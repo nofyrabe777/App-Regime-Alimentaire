6213b607a4bd632fafef3b8078c37ebd9c220506
ainatoky-tech <tokyaina797@gmail.com>
2026-05-08 00:57:52 +0200

    test

--- app/Models/ActiviteModel.php
+++ app/Models/ActiviteModel.php
@@ -7,4 +7,8 @@ class ActiviteModel extends Model{
     protected $table ='activites';
     protected $primaryKey = 'id_activite';
     protected $allowedFields = ['id_utilisateur','type_activite','duree','date_activite','frequence_jour','type_objetif'];
+
+    public function insertion($data){
+        return $this->insert();
+    }
 }
